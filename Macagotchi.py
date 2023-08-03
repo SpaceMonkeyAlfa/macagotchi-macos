df7c931733e9ccb1a38b77d8882b4ea6edfaae38
SpaceMonkeyAlfa <138901665+SpaceMonkeyAlfa@users.noreply.github.com>
2023-08-03 13:32:06 +0800

    Update Macagotchi.py

--- Macagotchi.py
+++ Macagotchi.py
@@ -172,6 +172,8 @@ while carryOn:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
                 wardriving = not wardriving
+                if scanTimes > 0:
+                        timer = 0
             # Check for backspace
             if event.key == pygame.K_F2:
                 typing = True
